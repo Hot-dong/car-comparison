d48225ee9b351c69568bd0885c5198c44385fe82
김동혁 <kdh1022255@gmail.com>
2023-06-01 13:42:20 +0900

    update

---
+++ src/test/java/comunity/carcomunity/CarComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package comunity.carcomunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarComunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
